087393cd3d03549a725fce3c378e75db67ed4fc8
Min-Yi <dra_goh@hotmail.com>
2016-10-20 12:02:15 +0800

    2nd hour progress

--- FinalAssessmentAug16IOS/FinalAssessmentAug16IOS/HomeViewController.swift
+++ FinalAssessmentAug16IOS/FinalAssessmentAug16IOS/HomeViewController.swift
@@ -10,38 +10,56 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    var listViewController: ListViewController?
+    
     var profiles = [Profile]()
+    var currentProfile: Profile?
+    var index = 0
     
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        listViewController = self.tabBarController?.viewControllers?[1] as? ListViewController
+        
         let user1 = Profile(username: "Isabelle", age: 28)
         let user2 = Profile(username: "Melissa", age: 25)
         let user3 = Profile(username: "Maya", age: 23)
         profiles.append(user1)
         profiles.append(user2)
         profiles.append(user3)
-        let currentProfile = profiles[0]
-        usernameLabel.text = currentProfile.username
-        ageLabel.text = String(currentProfile.age)
+        currentProfile = profiles[0]
+        usernameLabel.text = currentProfile!.username
+        ageLabel.text = String(describing: currentProfile!.age)
     }
     
     @IBAction func swipeGesture(recognizer: UISwipeGestureRecognizer) {
-        var index = 1
-        let currentProfile = profiles[index]
         switch recognizer.direction {
         case UISwipeGestureRecognizerDirection.right:
+            listViewController?.matchedProfiles.append(currentProfile!)
+            listViewController?.tableView?.reloadData()
             print("swiped to the right")
             index += 1
-            
+            if index >= profiles.count {
+                print("No more profiles to show")
+            } else {
+                currentProfile = profiles[index]
+                usernameLabel.text = currentProfile!.username
+                ageLabel.text = String(describing: currentProfile!.age)
+            }
         case UISwipeGestureRecognizerDirection.left:
             print("swiped to the left")
             index += 1
-            
+            if index >= profiles.count {
+                print("No more profiles to show")
+            } else {
+                currentProfile = profiles[index]
+                usernameLabel.text = currentProfile!.username
+                ageLabel.text = String(describing: currentProfile!.age)
+            }
         default:
             return
         }
--- FinalAssessmentAug16IOS/FinalAssessmentAug16IOS/ListViewController.swift
+++ FinalAssessmentAug16IOS/FinalAssessmentAug16IOS/ListViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var matchedProfiles = [Profile]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchedProfiles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        let profile = matchedProfiles[indexPath.row]
+        cell.textLabel?.text = profile.username
+        cell.detailTextLabel?.text = String(describing: profile.age)
+        return cell
     }
 
 }
